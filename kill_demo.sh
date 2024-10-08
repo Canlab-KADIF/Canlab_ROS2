2205b7d5bf1ed436a6fcb790b098ad0ad03e84db
ChoonSooKim <kcs@can-lab.co.kr>
2024-10-08 16:44:25 +0900

    add ros2 bag & ros2 launch in kill_demo.sh

--- kill_demo.sh
+++ kill_demo.sh
@@ -5,11 +5,25 @@ PASSWD=1
 if [ `whoami` == "root" ]; then
 	killall -2 clpe_ros
 	killall -2 nodelet
-	killall -2 ros2
+	bag=`ps -aux | grep "ros2 bag" | grep -v "color=auto"`
+	if [ $? == 0 ]; then
+		kill `ps -aux | grep "ros2 bag" | grep -v "color=auto" | awk '{print $2}'` 2>/dev/null
+	fi
+	launch=`ps -aux | grep "ros2 launch" | grep -v "color=auto"`
+	if [ $? == 0 ]; then
+		kill `ps -aux | grep "ros2 launch" | grep -v "color=auto" | awk '{print $2}'` 2>/dev/null
+	fi
 else
 	echo ${PASSWD} | sudo -S killall -2 clpe_ros
 	echo ${PASSWD} | sudo -S killall -2 nodelet
-	echo ${PASSWD} | sudo -S killall -2 ros2
+	bag=`ps -aux | grep "ros2 bag" | grep -v "color=auto"`
+	if [ $? == 0 ]; then
+		echo ${PASSWD} | sudo -S kill `ps -aux | grep "ros2 bag" | grep -v "color=auto" | awk '{print $2}'` 2>/dev/null
+	fi
+	launch=`ps -aux | grep "ros2 launch" | grep -v "color=auto"`
+	if [ $? == 0 ]; then
+		echo ${PASSWD} | sudo -S kill `ps -aux | grep "ros2 launch" | grep -v "color=auto" | awk '{print $2}'` 2>/dev/null
+	fi
 fi
 
 echo ' '
