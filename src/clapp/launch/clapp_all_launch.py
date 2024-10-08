94b09928dcfa98de89f48cb0cb139d9ac5699b2d
ChoonSooKim <kcs@can-lab.co.kr>
2024-10-08 15:38:14 +0900

    add clapp

---
+++ src/clapp/launch/clapp_all_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(package='clapp', executable='clapp_node', name='clapp_node', output='screen'),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([get_package_share_directory('clpe_ros'), '/launch', '/clpe_ros.launch.py'])
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([get_package_share_directory('ros2_socketcan'), '/launch', '/can.launch.py'])
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([get_package_share_directory('rslidar_sdk'), '/launch', '/start.py'])
+        )
+    ])
+
